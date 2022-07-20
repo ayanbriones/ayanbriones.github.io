f45809bface3acc0ec8a71f63934161d2593ed2e
Bryanth Briones <ayanbriones@gmail.com>
2022-07-21 01:42:01 +0300

    changed .gitignore

--- .next/static/webpack/webpack.55cebecf3e129d48.hot-update.js
+++ .next/static/webpack/webpack.55cebecf3e129d48.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "63d8052ae0ff64ec"; }
+/******/ 	__webpack_require__.h = function() { return "e4fb7ad9dea45c98"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
